8ac107a894013d6dc91bcf3dce6157b3eda62162
Johanna Fagerås <johannafageras88@gmail.com>
2025-12-04 08:18:56 +0100

    ..

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,9 +1,13 @@
-const pathPrefix = process.env.LOCAL ? '/' : '/stodlinjer/';
+const isLocal = process.env.LOCAL === '1';
+const pathPrefix = isLocal ? '/' : '/stodlinjer/';
 
 module.exports = function (eleventyConfig) {
   // Copy static assets
   eleventyConfig.addPassthroughCopy('src/assets');
   eleventyConfig.addPassthroughCopy({ 'src/_data': 'data' });
+
+  // Make baseUrl available in templates
+  eleventyConfig.addGlobalData('baseUrl', isLocal ? '' : '/stodlinjer');
 
   return {
     pathPrefix,
