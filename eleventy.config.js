42a9869d51cffbe162ab58de0c54c07d3c1593ef
Johanna Fagerås <johannafageras88@gmail.com>
2025-12-04 08:26:05 +0100

    ..

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,5 +1,7 @@
 const isLocal = process.env.LOCAL === '1';
-const pathPrefix = isLocal ? '/' : '/stodlinjer/';
+// Allow overriding the deployed subfolder. Default to root ('/'), set PATH_PREFIX=/stodlinjer for GitHub project pages.
+const pathPrefix = process.env.PATH_PREFIX || '/';
+const baseUrl = pathPrefix === '/' ? '' : pathPrefix.replace(/\/$/, '');
 
 module.exports = function (eleventyConfig) {
   // Copy static assets
@@ -7,7 +9,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy({ 'src/_data': 'data' });
 
   // Make baseUrl available in templates
-  eleventyConfig.addGlobalData('baseUrl', isLocal ? '' : '/stodlinjer');
+  eleventyConfig.addGlobalData('baseUrl', baseUrl);
 
   return {
     pathPrefix,
